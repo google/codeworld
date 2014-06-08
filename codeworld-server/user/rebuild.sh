8633bff9634a3b59b7917cc9ac8b90038841d1c5
Chris Smith <cdsmith@gmail.com>
2014-06-08 17:52:32 +0000

    Delete obsolete rebuild script.  No longer needed.

--- codeworld-server/user/rebuild.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ls *.hs | sed s/\\.hs// \
-        | awk '{print "rm -rf "$1".jsexe ; ghcjs -hide-package base -package codeworld-base -XRebindableSyntax -XImplicitPrelude -XOverloadedStrings "$1".hs"}' \
-        | /bin/sh

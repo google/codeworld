efbc6a48333e2ddbdac7ba8ac341522797407905
Chris Smith <cdsmith@google.com>
2014-06-17 23:29:56 -0700

    Add copyright headers to all source code.

--- codeworld-base/Internal/DeepEq.hs
+++ codeworld-base/Internal/DeepEq.hs
@@ -6,6 +6,22 @@
 {-# LANGUAGE PackageImports           #-}
 
 {-
+  Copyright 2014 Google Inc. All rights reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+-}
+
+{-
     The really hairy bits here are written by luite.  Note that this
     code is very dependent on the internals of GHCJS, so complain if
     this is broken.

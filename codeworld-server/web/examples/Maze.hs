f1133cddd86e820847a4ed362e6a243d14b94ce2
joshcough <joshcough@gmail.com>
2014-08-16 23:23:57 -0400

    removed redundant line

--- codeworld-server/web/examples/Maze.hs
+++ codeworld-server/web/examples/Maze.hs
@@ -13,7 +13,6 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 ----- END LICENSE TEXT -----}
-{- Random Maze Generator -}
 {-
     Example: Maze
 

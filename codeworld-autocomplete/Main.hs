d479df866dc8e7ff97fe1814e70f52346da7756f
Chris Smith <cdsmith@google.com>
2014-09-05 21:11:07 -0700

    Improve matching to generate autocomplete list. (fixes #75)

--- codeworld-autocomplete/Main.hs
+++ codeworld-autocomplete/Main.hs
@@ -14,6 +14,8 @@
   limitations under the License.                                                                                                            
 -}
 
+{-# LANGUAGE BangPatterns #-}
+
 import Control.Applicative
 import Data.List
 import System.Environment
@@ -24,8 +26,16 @@ main :: IO ()
 main = do
     [symbolFile] <- getArgs
     decls <- mergeContinued <$> lines <$> readFile symbolFile
-    let tokens = concatMap tokensFrom decls
+    let preludeDecls = restrictToPrelude decls
+    let tokens = concatMap tokensFrom preludeDecls
     mapM_ putStrLn (sort $ nub $ tokens)
+
+restrictToPrelude :: [String] -> [String]
+restrictToPrelude = takeWhile (not . isModule)
+                  . tail
+                  . dropWhile (not . isPrelude)
+  where isModule  decl = decl =~ "^module .*$" :: Bool
+        isPrelude decl = decl == "module Prelude"
 
 mergeContinued :: [String] -> [String]
 mergeContinued []      = []
@@ -35,14 +45,38 @@ mergeContinued (l1:l2:r)
     | otherwise        = l1 : mergeContinued (l2:r)
 
 tokensFrom :: String -> [String]
-tokensFrom decl = concatMap (everyOther . drop 2) (decl =~ expr :: [[String]])
-  where expr = "^((([A-Za-z0-9_]*) :: .*)"
-            ++ "|(\\(([^)]*)\\) :: .*)"
-            ++ "|(data ([A-Za-z0-9_]*) .*)"
-            ++ "|(newtype ([A-Za-z0-9_]*) .*)"
-            ++ "|(type ([A-Za-z0-9_]*) .*))"
-            ++ "$"
+tokensFrom decl = tokensFromOperator decl ++
+                  tokensFromIdentifier decl ++
+                  tokensFromData decl ++
+                  tokensFromNewtype decl ++
+                  tokensFromType decl
 
-everyOther :: [a] -> [a]
-everyOther (a:b:c) = b : everyOther c
-everyOther _       = []
+tokensFromOperator :: String -> [String]
+tokensFromOperator decl = concatMap (elemsAt [1]) matches
+  where matches = decl =~ "^\\(([^)]*)\\) :: .*$" :: [[String]]
+
+tokensFromIdentifier :: String -> [String]
+tokensFromIdentifier decl = concatMap (elemsAt [1]) matches
+  where matches = decl =~ "^([A-Za-z0-9_]*) :: .*$" :: [[String]]
+
+tokensFromData :: String -> [String]
+tokensFromData decl = concatMap (elemsAt [1]) matches
+  where matches = decl =~ "^data ([A-Za-z0-9_]*)( .*)?$" :: [[String]]
+
+tokensFromNewtype :: String -> [String]
+tokensFromNewtype decl = concatMap (elemsAt [1]) matches
+  where matches = decl =~ "^newtype ([A-Za-z0-9_]*)( .*)?$" :: [[String]]
+
+tokensFromType :: String -> [String]
+tokensFromType decl = concatMap (elemsAt [1]) matches
+  where matches = decl =~ "^newtype ([A-Za-z0-9_]*)( .*)?$" :: [[String]]
+
+-- Selects specific indices from a list.  The list of indices must be
+-- in non-decreasing order.
+elemsAt :: [Int] -> [a] -> [a]
+elemsAt is xs = go xs is 0
+  where go []     _      _  = []
+        go _      []     _  = []
+        go (x:xs) (i:is) !j -- invariant: i >= j
+          | i == j          = x : go (x:xs) is j
+          | otherwise       = go xs (i:is) (j+1)

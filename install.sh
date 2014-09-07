c1a6601c80fca112ae07f7414c33d13d32d17574
Chris Smith <cdsmith@gmail.com>
2014-09-07 05:52:14 +0000

    Separate install and run steps.

--- install.sh
+++ install.sh
@@ -155,26 +155,3 @@ git clone https://github.com/ghcjs/ghcjs-dom.git $BUILD/ghcjs-dom
 git clone https://github.com/ghcjs/ghcjs-canvas $BUILD/ghcjs-canvas
 (cd $BUILD/ghcjs-canvas && cabal install --global --ghcjs --prefix=$BUILD)
 
-# Install the codeworld-base package
-
-(cd codeworld-base &&  cabal install --global --ghcjs --prefix=$BUILD)
-(cd codeworld-base &&  cabal haddock)
-
-# Build and run the autocomplete generator.
-
-(cd codeworld-autocomplete &&  cabal install --global --prefix=$BUILD --dependencies-only)
-(cd codeworld-autocomplete &&  cabal build)
-(cd codeworld-autocomplete &&  ./run.sh)
-
-# Build codeworld-server from this project: cd codeworld-server && cabal build
-
-(cd codeworld-server &&  cabal install --global --prefix=$BUILD --dependencies-only)
-(cd codeworld-server &&  cabal build)
-
-# Get a Google API key, and store it in web/clientId.txt.
-
-echo MYGOOGLEAPIKEY > codeworld-server/web/clientId.txt
-
-# Run the server: cd codeworld-server && ./run.sh 8080.
-
-(cd codeworld-server && ./run.sh 8080)

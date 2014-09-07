dcf0fd2931bef4b0c60fff3a9f875bffcc68d922
Chris Smith <cdsmith@gmail.com>
2014-09-07 02:32:04 +0000

    Fix install script

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "This script does not function properly yet!  Please follow the"
 echo "instructions in the README file."
+
+echo "Continue anyway?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No  ) exit;;
+    esac
+done
 
 BUILD=$(pwd)/build
 DOWNLOADS=$BUILD/downloads
@@ -26,16 +34,17 @@ then
   sudo yum install -y ncurses-devel
 
   # Needed for GHC 7.8
-
   sudo yum install -y gcc
   sudo yum install -y gmp-devel
 
-  # Needed for ghcjs-boot --dev
+  # Needed for nodejs
+  sudo yum install -y gcc-c++
+  sudo yum install -y openssl-devel
 
+  # Needed for ghcjs-boot --dev
   sudo yum install -y patch
   sudo yum install -y autoconf
   sudo yum install -y automake
-  sudo yum install -y nodejs
 
   # Choose the right GHC 7.8.2 download
   GHC_ARCH=x86_64-unknown-linux-centos65
@@ -50,17 +59,19 @@ then
   sudo apt-get install -y libncurses5-dev
 
   # Needed for GHC 7.8
-
   sudo apt-get install -y make
   sudo apt-get install -y gcc
   sudo apt-get install -y libgmp-dev
 
   # Needed for ghcjs-boot --dev
-
   sudo apt-get install -y patch
   sudo apt-get install -y autoconf
   sudo apt-get install -y automake
   sudo apt-get install -y nodejs-legacy
+
+  # Needed for nodejs
+  sudo apt-get install -y g++
+  sudo apt-get install -y openssl
 
   # Choose the right GHC 7.8.2 download
   GHC_ARCH=x86_64-unknown-linux-deb7
@@ -68,6 +79,14 @@ else
   echo "WARNING: Could not find package manager."
   echo "Make sure necessary packages are installed."
 fi
+
+# install node (necessary for ghcjs-boot)
+
+(cd $DOWNLOADS && wget http://nodejs.org/dist/v0.10.29/node-v0.10.29.tar.gz)
+(cd $BUILD && tar -zxf $DOWNLOADS/node-v0.10.29.tar.gz)
+(cd $BUILD/node-v0.10.29 && ./configure --prefix=$BUILD)
+(cd $BUILD/node-v0.10.29 && make)
+(cd $BUILD/node-v0.10.29 && make install)
 
 # Install GHC 7.8, since it's required for GHCJS.
 
@@ -83,7 +102,7 @@ fromHackage() {
     (cd $BUILD && tar -xzf $DOWNLOADS/$1-$2.tar.gz)
     (cd $BUILD/$1-$2 && runghc $3 configure --prefix=$BUILD)
     (cd $BUILD/$1-$2 && runghc $3 build)
-    (cd $BUILD/$1-$2 && runghc $3 install)
+    (cd $BUILD/$1-$2 && runghc $3 install --global)
 }
 
 fromHackage zlib 0.5.4.1 Setup.hs
@@ -99,15 +118,13 @@ fromHackage HTTP 4000.2.17 Setup.lhs
 # then git checkout ghcjs to switch to the GHCJS branch, and finally
 # cabal install both the Cabal and cabal-install packages.
 
-
 fromLocal() {
-    (cd $1 && runghc $2 configure --prefix=$BUILD)
-    (cd $1 && runghc $2 build)
-    (cd $1 && runghc $2 install)
+    (cd $1 && runghc Setup.hs configure --prefix=$BUILD)
+    (cd $1 && runghc Setup.hs build)
+    (cd $1 && runghc Setup.hs install --global)
 }
 
 git clone -b ghcjs https://github.com/ghcjs/cabal.git $BUILD/cabal
-(cd $BUILD/cabal && git checkout f70ed6277942c74bdd68f63e2b0694d57dcb8606) # Current ghcjs/HEAD doesn't build
 fromLocal $BUILD/cabal/Cabal Setup.hs
 fromLocal $BUILD/cabal/cabal-install Setup.hs
 
@@ -115,16 +132,14 @@ cabal update
 
 # Get GHCJS itself (https://github.com/ghcjs/ghcjs) and cabal install.
 
-cabal install --prefix=$BUILD alex
-cabal install --prefix=$BUILD happy
+cabal install --global --prefix=$BUILD alex
+cabal install --global --prefix=$BUILD happy
 
 git clone https://github.com/ghcjs/ghcjs-prim.git $BUILD/ghcjs-prim
-(cd $BUILD/ghcjs-prim && git checkout 54d2ce21d713fe052dcad8e815e13c13585a4262)
-(cd $BUILD/ghcjs-prim && cabal install --prefix=$BUILD)
+(cd $BUILD/ghcjs-prim && cabal install --global --prefix=$BUILD)
 
 git clone https://github.com/ghcjs/ghcjs.git $BUILD/ghcjs
-(cd $BUILD/ghcjs && git checkout e7d77288517e202708187fe6bcca2eead7aefa6c)
-(cd $BUILD/ghcjs && cabal install --prefix=$BUILD)
+(cd $BUILD/ghcjs && cabal install --global --prefix=$BUILD)
 
 # Bootstrap ghcjs
 
@@ -133,27 +148,27 @@ ghcjs-boot --dev
 # Check out ghcjs-dom (https://github.com/ghcjs/ghcjs-dom) and install it
 
 git clone https://github.com/ghcjs/ghcjs-dom.git $BUILD/ghcjs-dom
-(cd $BUILD/ghcjs-dom && git checkout 1fb18971dff6a793de27d95b4561411ff0f9c722)
-(cd $BUILD/ghcjs-dom && cabal install --ghcjs --prefix=$BUILD)
+(cd $BUILD/ghcjs-dom && cabal install --global --ghcjs --prefix=$BUILD)
 
 # Check out ghcjs-canvas (https://github.com/ghcjs/ghcjs-canvas) and install it
 
 git clone https://github.com/ghcjs/ghcjs-canvas $BUILD/ghcjs-canvas
-(cd $BUILD/ghcjs-canvas && git checkout 73a09bfc538b61f05299f58dedb5f3010437efcc)
-(cd $BUILD/ghcjs-canvas && cabal install --ghcjs --prefix=$BUILD)
+(cd $BUILD/ghcjs-canvas && cabal install --global --ghcjs --prefix=$BUILD)
 
 # Install the codeworld-base package
 
-(cd codeworld-base &&  cabal install --ghcjs --prefix=$BUILD)
+(cd codeworld-base &&  cabal install --global --ghcjs --prefix=$BUILD)
+(cd codeworld-base &&  cabal haddock)
 
 # Build and run the autocomplete generator.
 
-(cd codeworld-autocomplete &&  cabal install --prefix=$BUILD --dependencies-only)
+(cd codeworld-autocomplete &&  cabal install --global --prefix=$BUILD --dependencies-only)
+(cd codeworld-autocomplete &&  cabal build)
 (cd codeworld-autocomplete &&  ./run.sh)
 
 # Build codeworld-server from this project: cd codeworld-server && cabal build
 
-(cd codeworld-server &&  cabal install --prefix=$BUILD --dependencies-only)
+(cd codeworld-server &&  cabal install --global --prefix=$BUILD --dependencies-only)
 (cd codeworld-server &&  cabal build)
 
 # Get a Google API key, and store it in web/clientId.txt.

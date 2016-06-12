8170d951383e71661fe53e37a6702248a5aef223
Chris Smith <cdsmith@gmail.com>
2016-06-12 21:39:47 +0000

    Update copyrights and add missing license notices.

--- web/js/blockworld.js
+++ web/js/blockworld.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /*
  * Utility function for sending an HTTP request to fetch a resource.
  *

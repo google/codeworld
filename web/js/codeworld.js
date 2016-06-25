41a8f381b4d287ba379d076acc29ffe5c5f13fcc
Chris Smith <cdsmith@gmail.com>
2016-06-25 23:35:20 +0000

    Update help() to take a parameter for different page names.
    This is speculative, but I imagine it could be very helpful.

--- web/js/codeworld.js
+++ web/js/codeworld.js
@@ -436,10 +436,12 @@ function toggleBrowser() {
     updateUI();
 }
 
-function help() {
+function help(doc) {
+    if (!doc) doc = window.buildMode;
+    var url = 'doc.html?help/' + doc + '.md';
     sweetAlert({
         title: '',
-        text: '<iframe id="doc" style="width: 100%; height: 100%" class="dropbox" src="doc.html?help/' + window.buildMode + '.md"></iframe>',
+        text: '<iframe id="doc" style="width: 100%; height: 100%" class="dropbox" src="' + url + '"></iframe>',
         html: true,
         customClass: 'helpdoc',
         allowEscapeKey: true,

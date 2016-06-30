5258e58d3f873543b916558bc8d2bad606f9893d
Chris Smith <cdsmith@gmail.com>
2016-06-30 04:19:26 +0000

    Switch to a hand-written CodeWorld mode.
    This will make it easier to implement new highlighting features
    in the future.  In particular, rainbow brackets are not hard now,
    and coming in a commit soon.

--- web/js/codeworld-mode.js
+++ web/js/codeworld-mode.js
@@ -22,204 +22,143 @@
 "use strict";
 
 CodeMirror.defineMode("codeworld", function(_config, modeConfig) {
-  var smallRE = /[a-z_]/;
-  var largeRE = /[A-Z]/;
-  var digitRE = /\d/;
-  var hexitRE = /[0-9A-Fa-f]/;
-  var octitRE = /[0-7]/;
-  var idRE = /[a-z_A-Z0-9']/;
-  var symbolRE = /[-!#$%&*+.\/<=>?@\\^|~:]/;
-  var specialRE = /[(),;[\]`{}]/;
-  var whiteCharRE = /[ \t\v\f]/;
+  // This is a regular expression used in multiple contexts.
+  var MULTICHAR_ESCAPE_REGEX =
+      '\\\\NUL|\\\\SOH|\\\\STX|\\\\ETX|\\\\EOT|\\\\ENQ|\\\\ACK|\\\\BEL|\\\\BS|' +
+      '\\\\HT|\\\\LF|\\\\VT|\\\\FF|\\\\CR|\\\\SO|\\\\SI|\\\\DLE|\\\\DC1|\\\\DC2|' +
+      '\\\\DC3|\\\\DC4|\\\\NAK|\\\\SYN|\\\\ETB|\\\\CAN|\\\\EM|\\\\SUB|\\\\ESC|' +
+      '\\\\FS|\\\\GS|\\\\RS|\\\\US|\\\\SP|\\\\DEL';
 
-  function normal(source, setState) {
-    if (source.eatWhile(whiteCharRE)) {
-      return null;
-    }
+  var RE_WHITESPACE   = /[ \v\t\f]+/;
+  var RE_STARTMETA    = /{-#/;
+  var RE_STARTCOMMENT = /{-/;
+  var RE_DASHES       = /--+(?=$|[^:!#$%&*+.\/<=>?@\\^|~-]+)/;
+  var RE_QUAL         = /[A-Z][A-Za-z_0-9']*\.(?=[A-Za-z_:!#$%&*+.\/<=>?@\\^|~]|-[^-])/;
+  var RE_VARID        = /[a-z_][A-Za-z_0-9']*/;
+  var RE_CONID        = /[A-Z][A-Za-z_0-9']*/;
+  var RE_VARSYM       = /[!#$%&*+.\/<=>?@\\^|~-][:!#$%&*+.\/<=>?@\\^|~-]*/;
+  var RE_CONSYM       = /:[:!#$%&*+.\/<=>?@\\^|~-]*/;
+  var RE_NUMBER       = /[0-9]+(?:\.[0-9]+)?(?:[eE][+-]?[0-9]+)?|0[oO][0-7]+|0[xX][0-9a-fA-F]+/;
+  var RE_CHAR         = new RegExp('\'(?:[^\\\\\']|\\\\[abfnrtv\\\\"\']|\\\\^[A-Z@[\\\\\\]^_]|' +
+                                   MULTICHAR_ESCAPE_REGEX + ')\'');
+  var RE_STRING       = new RegExp('"(?:[^\\\\"]|\\\\[abfnrtv\\\\"\'&]|\\\\^[A-Z@[\\\\\\]^_]|' +
+                                   MULTICHAR_ESCAPE_REGEX + ')*"');
+  var RE_OPENBRACKET  = /[([{]/;
+  var RE_CLOSEBRACKET  = /[)\]}]/;
+  var RE_INCOMMENT    = /(?:[^{-]|-(?=[^}])|\{(?=[^-]))*/;
+  var RE_ENDCOMMENT   = /-}/;
 
-    var ch = source.next();
-    if (specialRE.test(ch)) {
-      if (ch == '{' && source.eat('-')) {
-        var t = "comment";
-        if (source.eat('#')) {
-          t = "meta";
-        }
-        setState(ncomment(t, 1));
-        return (ncomment(t, 1))(source, setState);
-      }
-      return null;
-    }
-
-    if (ch == '\'') {
-      if (source.eat('\\')) {
-        source.next();
-      }
-      else {
-        source.next();
-      }
-      if (source.eat('\'')) {
-        return "string";
-      }
-      return "error";
-    }
-
-    if (ch == '"') {
-      setState(stringLiteral);
-      return stringLiteral(source, setState);
-    }
-
-    if (largeRE.test(ch)) {
-      source.eatWhile(idRE);
-      if (source.eat('.')) {
-        return "qualifier";
-      }
-      return "variable-2";
-    }
-
-    if (smallRE.test(ch)) {
-      source.eatWhile(idRE);
-      return "variable";
-    }
-
-    if (digitRE.test(ch)) {
-      if (ch == '0') {
-        if (source.eat(/[xX]/)) {
-          source.eatWhile(hexitRE);
-          return "number";
-        }
-        if (source.eat(/[oO]/)) {
-          source.eatWhile(octitRE);
-          return "number";
-        }
-      }
-      source.eatWhile(digitRE);
-      var t = "number";
-      if (source.match(/^\.\d+/)) {
-        t = "number";
-      }
-      if (source.eat(/[eE]/)) {
-        t = "number";
-        source.eat(/[-+]/);
-        source.eatWhile(digitRE);
-      }
-      return t;
-    }
-
-    if (ch == "." && source.eat("."))
-      return "keyword";
-
-    if (symbolRE.test(ch)) {
-      if (ch == '-' && source.eat(/-/)) {
-        source.eatWhile(/-/);
-        if (!source.eat(symbolRE)) {
-          source.skipToEnd();
-          return "comment";
-        }
-      }
-      var t = "variable";
-      if (ch == ':') {
-        t = "variable-2";
-      }
-      source.eatWhile(symbolRE);
-      return t;
-    }
-
-    return "error";
+  function closing(bracket) {
+    if (bracket == '(') return ')';
+    if (bracket == '[') return ']';
+    if (bracket == '{') return '}';
+    if (bracket == '`') return '`';
   }
 
-  function ncomment(type, nest) {
-    if (nest == 0) {
-      return normal;
+  // The state has the following properties:
+  //
+  // func:          The function to tokenize the remaining stream.
+  // commentLevel:  Number of levels of block comments.
+  // brackets:      Brackets, from outermost to innermost.
+
+  function normal(stream, state) {
+    if (stream.match(RE_WHITESPACE)) return null;
+
+    if (stream.match(RE_STARTMETA)) {
+      state.func = blockComment('meta');
+      ++state.commentLevel;
+      return state.func(stream, state);
     }
-    return function(source, setState) {
-      var currNest = nest;
-      while (!source.eol()) {
-        var ch = source.next();
-        if (ch == '{' && source.eat('-')) {
-          ++currNest;
-        }
-        else if (ch == '-' && source.eat('}')) {
-          --currNest;
-          if (currNest == 0) {
-            setState(normal);
-            return type;
-          }
-        }
+
+    if (stream.match(RE_STARTCOMMENT)) {
+      state.func = blockComment('comment');
+      ++state.commentLevel;
+      return state.func(stream, state);
+    }
+
+    if (stream.match(RE_DASHES)) {
+      stream.skipToEnd();
+      return 'comment';
+    }
+
+    if (stream.match(RE_QUAL)) return 'qualifier';
+    if (stream.match(RE_VARID) || stream.match(RE_VARSYM)) return 'variable';
+    if (stream.match(RE_CONID) || stream.match(RE_CONSYM)) return 'variable-2';
+    if (stream.match(RE_NUMBER)) return 'number';
+    if (stream.match(RE_CHAR) || stream.match(RE_STRING)) return 'string';
+
+    if (stream.match(RE_OPENBRACKET)) {
+      return 'bracket';
+    }
+
+    if (stream.match(RE_CLOSEBRACKET)) {
+      return 'bracket';
+    }
+
+    stream.next();
+    return null;
+  }
+
+  function blockComment(tokenType) {
+    return function(stream, state) {
+      if (state.commentLevel == 0) {
+        state.func = normal;
+        return tokenType;
       }
-      setState(ncomment(type, currNest));
-      return type;
+
+      stream.match(RE_INCOMMENT);
+      if (stream.match(RE_STARTCOMMENT)) {
+        ++state.commentLevel;
+        return state.func(stream, state);
+      }
+      if (stream.match(RE_ENDCOMMENT)) {
+        --state.commentLevel;
+        return state.func(stream, state);
+      }
+      return tokenType;
     };
   }
 
-  function stringLiteral(source, setState) {
-    while (!source.eol()) {
-      var ch = source.next();
-      if (ch == '"') {
-        setState(normal);
-        return "string";
-      }
-      if (ch == '\\') {
-        if (source.eol() || source.eat(whiteCharRE)) {
-          setState(stringGap);
-          return "string";
-        }
-        if (source.eat('&')) {
-        }
-        else {
-          source.next();
-        }
-      }
-    }
-    setState(normal);
-    return "error";
-  }
-
-  function stringGap(source, setState) {
-    if (source.eat('\\')) {
-      setState(stringLiteral);
-      return stringLiteral(source, setState);
-    }
-    source.next();
-    setState(normal);
-    return "error";
-  }
-
   var wellKnownWords = (function() {
-    var wkw = {};
-    function setType(t) {
-      return function () {
-        for (var i = 0; i < arguments.length; i++)
-          wkw[arguments[i]] = t;
-      };
-    }
+    var result = {};
 
-    setType("keyword")(
-      "case", "class", "data", "default", "deriving", "do", "else", "foreign",
-      "if", "import", "in", "infix", "infixl", "infixr", "instance", "let",
-      "module", "newtype", "of", "then", "type", "where", "_");
+    var keywords = [
+      'case', 'class', 'data', 'default', 'deriving', 'do', 'else', 'foreign',
+      'if', 'import', 'in', 'infix', 'infixl', 'infixr', 'instance', 'let',
+      'module', 'newtype', 'of', 'then', 'type', 'where', '_', '..', ':',
+      '=', '::', '\\', '<-', '->', '@', '~', '=>', '|'
+    ];
 
-    setType("keyword")(
-      "\.\.", ":", "::", "=", "\\", "\"", "<-", "->", "@", "~", "=>", "|");
+    for (var i = 0; i < keywords.length; ++i) result[keywords[i]] = 'keyword';
 
     var override = modeConfig.overrideKeywords;
     if (override) for (var word in override) if (override.hasOwnProperty(word))
-      wkw[word] = override[word];
+      result[word] = override[word];
 
-    return wkw;
+    return result;
   })();
 
   return {
-    startState: function ()  { return { f: normal }; },
-    copyState:  function (s) { return { f: s.f }; },
+    startState: function() {
+      return {
+        func: normal,
+        commentLevel: 0,
+        brackets: []
+      };
+    },
 
     token: function(stream, state) {
-      var t = state.f(stream, function(s) { state.f = s; });
-      var w = stream.current();
-      return wellKnownWords.hasOwnProperty(w) ? wellKnownWords[w] : t;
+      var t = state.func(stream, state);
+      if ([ 'variable', 'variable-2' ].indexOf(t) != -1) {
+        var w = stream.current();
+        if (wellKnownWords.hasOwnProperty(w)) return wellKnownWords[w];
+      }
+      return t;
     },
 
     blockCommentStart: "{-",
     blockCommentEnd: "-}",
-    lineComment: "--"
+    lineComment: "--",
+    indent: null
   };
 });

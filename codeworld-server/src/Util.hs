acbf8fc8b143858209f1894a49db9bd8ec7823ff
Pranjal Tale <pranjaltale16@gmail.com>
2017-06-15 16:35:23 +0530

    Added function to save raw error file in Util

--- codeworld-server/src/Util.hs
+++ codeworld-server/src/Util.hs
@@ -70,6 +70,9 @@ targetFile programId = sourceBase programId <.> "js"
 resultFile :: ProgramId -> FilePath
 resultFile programId = sourceBase programId <.> "err.txt"
 
+rawResultFile :: ProgramId -> FilePath
+rawResultFile programId = sourceBase programId <.> "rawerr.txt"
+
 auxiliaryFiles :: ProgramId -> [FilePath]
 auxiliaryFiles programId = [
     sourceBase programId <.> "js_hi",

2265790f9f0a79094a9e57eb77c191ae2421ec9f
Chris Smith <cdsmith@gmail.com>
2017-09-01 05:13:21 +0000

    Remove extraneous newlines in stdout / stderr.
    Fixes #538

--- web/js/run.js
+++ web/js/run.js
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-function reportRuntimeError(err, str) {
+function addMessage(err, str) {
   if (window.parent && window.parent.addToMessage) {
-    var message = window.parent.addToMessage('\n' + str);
+    var message = window.parent.addToMessage(str);
 
     if (err) {
       var message = window.parent.document.getElementById('message');
@@ -44,23 +44,23 @@ function showCanvas() {
 
 function start() {
   h$base_writeStdout = function(fd, fdo, buf, buf_offset, n, c) {
-    reportRuntimeError(false, h$decodeUtf8(buf, n, buf_offset));
+    addMessage(false, h$decodeUtf8(buf, n, buf_offset));
     c(n);
   };
   h$base_writeStderr = function(fd, fdo, buf, buf_offset, n, c) {
-    reportRuntimeError(false, h$decodeUtf8(buf, n, buf_offset));
+    addMessage(false, h$decodeUtf8(buf, n, buf_offset));
     c(n);
   };
   h$log = function() {
     var s = '';
     for(var i=0;i<arguments.length;i++) { s = s + arguments[i]; }
-    reportRuntimeError(false, s+'\n');
+    addMessage(false, s+'\n');
   };
   h$errorMsg = function(str) {
     for(var i=1;i<arguments.length;i++) {
       str = str.replace(/%s/, arguments[i]);
     }
-    reportRuntimeError(true, str);
+    addMessage(true, '\n' + str);
   };
   h$base_stdout_fd.write = h$base_writeStdout;
   h$base_stderr_fd.write = h$base_writeStderr;

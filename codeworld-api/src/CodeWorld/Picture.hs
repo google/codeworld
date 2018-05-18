e54c7f7b5fead586358ba303fc3da8cda06bc259
kammitama5 <krystal.maughan@gmail.com>
2018-05-17 22:39:48 -0700

    Add SolidClosedCurve (#624)
    * Add SolidClosedCurve
    * Change S to s to keep consistency
    * change Bool to reflect Functions in Picture.hs
    * Resolve Driver.hs types
    * Omit space before close parenthesis
    * Remove unnecessary spaces in getPictureCS
    * Change SolidPolygon in solidLoop to SolidClosedCurve
    * Fix reversed arguments for SolidClosedCurve and SolidPolygon
    
    Fixes #621

--- codeworld-api/src/CodeWorld/Picture.hs
+++ codeworld-api/src/CodeWorld/Picture.hs
@@ -50,7 +50,8 @@ dotProduct (x1, y1) (x2, y2) = x1 * x2 + y1 * y2
 data Picture
     = SolidPolygon CallStack
               [Point]
-              !Bool
+    | SolidClosedCurve CallStack
+              [Point]
     | Path CallStack
            [Point]
            !Double
@@ -135,7 +136,7 @@ thickPolygon n ps = Path callStack ps n True False
 
 -- | A solid polygon with these points as vertices
 solidPolygon :: HasCallStack => [Point] -> Picture
-solidPolygon ps = SolidPolygon callStack ps False
+solidPolygon ps = SolidPolygon callStack ps
 
 -- | A smooth curve passing through these points.
 curve :: HasCallStack => [Point] -> Picture
@@ -167,11 +168,11 @@ thickLoop n ps = Path callStack ps n True True
 
 -- | A solid smooth closed curve passing through these points.
 solidClosedCurve :: HasCallStack => [Point] -> Picture
-solidClosedCurve ps = SolidPolygon callStack ps True
+solidClosedCurve ps = SolidClosedCurve callStack ps
 
 -- | A solid smooth closed curve passing through these points.
 solidLoop :: HasCallStack => [Point] -> Picture
-solidLoop ps = SolidPolygon callStack ps True
+solidLoop ps = SolidClosedCurve callStack ps
 
 {-# WARNING solidLoop "Please use solidClosedCurve instead of solidLoop." #-}
 

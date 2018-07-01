d7dc046d0ebf8cf92ff023e5fc21e7a1302759f2
Krystal Maughan <krystal.maughan@gmail.com>
2018-07-01 13:58:23 -0700

    does not typecheck

--- codeworld-api/src/CodeWorld/Driver.hs
+++ codeworld-api/src/CodeWorld/Driver.hs
@@ -2350,7 +2350,9 @@ drawControl w alpha TimeLabel = translated 8 (-9) p
 drawControl w alpha SpeedSlider = translated (-4) (-9) p
   where
     p =
-    
+        colored
+            (RGBA 0 0 0 alpha)
+            (scaled 0.5 0.5 $ lettering (pack (showFFloatAlt (Just 2) (state w) ""))) <>
         translated x 0 (solidRectangle 0.2 0.8) <>
         colored (RGBA 0.2 0.2 0.2 alpha) (rectangle 2.8 0.6) <>
         colored (RGBA 0.8 0.8 0.8 alpha) (solidRectangle 2.8 0.6)

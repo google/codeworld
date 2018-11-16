4000a17bdcc96eb4868b276aef4d9e0e2d5ff69a
Chris Smith <cdsmith@gmail.com>
2018-11-16 04:04:17 +0000

    Refactor requirements extraction.  Also move failure to parse a requirement from the reqs summary to a new diagnostic of its own.

--- codeworld-compiler/src/CodeWorld/Compile/Requirements.hs
+++ codeworld-compiler/src/CodeWorld/Compile/Requirements.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -25,29 +26,50 @@ import CodeWorld.Compile.Requirements.Language
 import Data.Array
 import Data.Text (Text)
 import qualified Data.Text as T
-import Language.Haskell.Exts (Module, SrcSpanInfo, noSrcSpan)
+import Language.Haskell.Exts
 import Text.Regex.TDFA
 import Text.Regex.TDFA.Text
+
+requirementsDiagnostics :: SourceMode -> Text -> ParsedCode -> [Diagnostic]
+requirementsDiagnostics _ src m
+  | null reqs = diags
+  | otherwise = (noSrcSpan, Info,
+                 ":: REQUIREMENTS ::\n" ++
+                 concatMap (handleRequirement m) reqs ++
+                 ":: END REQUIREMENTS ::\n") : diags
+  where (diags, reqs) = extractRequirements src
 
 pattern :: Text
 pattern = "{-+[[:space:]]*(REQUIRES\\b(\n|[^-]|-[^}])*)-}"
 
-requirementsDiagnostics :: SourceMode -> Text -> ParsedCode -> [Diagnostic]
-requirementsDiagnostics _ src m
-  | hasReqs   = [(noSrcSpan, Info, response)]
-  | otherwise = []
-  where reqs = [ parseRequirement $ T.take len (T.drop off src)
-                 | matchArray :: MatchArray <- src =~ pattern
-                 , rangeSize (bounds matchArray) > 1
-                 , let (off, len) = matchArray ! 1 ]
-        hasReqs = not (null (reqs))
-        response = ":: REQUIREMENTS ::\n" ++
-                   concatMap (handleRequirement m) reqs ++
-                   ":: END REQUIREMENTS ::\n"
+extractRequirements :: Text -> ([Diagnostic], [Requirement])
+extractRequirements src = (diags, reqs)
+  where results = [ (srcSpanFor src off len,
+                     parseRequirement (T.take len (T.drop off src)))
+                    | matchArray :: MatchArray <- src =~ pattern
+                    , rangeSize (bounds matchArray) > 1
+                    , let (off, len) = matchArray ! 1 ]
+        diags = [ (srcSpan, Warning,
+                   "The requirement could not be understood:\n" ++ err)
+                  | (srcSpan, Left err) <- results ]
+        reqs = [ req | (_, Right req) <- results ]
 
-handleRequirement :: ParsedCode -> Either String Requirement -> String
-handleRequirement m (Left err) = "[?] A requirement could not be understood\n" ++ err
-handleRequirement m (Right r) = label ++ desc ++ "\n" ++ concat [ msg ++ "\n" | msg <- msgs ]
+handleRequirement :: ParsedCode -> Requirement -> String
+handleRequirement m r =
+    label ++ desc ++ "\n" ++ concat [ "    " ++ msg ++ "\n" | msg <- msgs ]
   where (desc, success, msgs) = evalRequirement r m
         label | success   = "[Y] "
               | otherwise = "[N] "
+
+srcSpanFor :: Text -> Int -> Int -> SrcSpanInfo
+srcSpanFor src off len =
+    SrcSpanInfo (SrcSpan "program.hs" ln1 col1 ln2 col2) []
+  where (ln1, col1) = advance 0 off 1 1
+        (ln2, col2) = advance off len ln1 col1
+
+        advance _ !0 !ln !col = (ln, col)
+        advance !i !n !ln !col = case T.index src n of
+            '\r' -> advance (i+1) (n-1) ln col
+            '\n' -> advance (i+1) (n-1) (ln + 1) 1
+            '\t' -> advance (i+1) (n-1) ln (col + 8 - (mod (col - 1) 8))
+            _    -> advance (i+1) (n-1) ln (col + 1)

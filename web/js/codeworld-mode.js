90d81ff360dc8605de1719344405ab1fca15ccf0
Artem Kanev <a-kanev@yandex.ru>
2019-03-14 15:20:55 +0300

    Add layout markers into codeworld-mode

--- web/js/codeworld-mode.js
+++ web/js/codeworld-mode.js
@@ -52,6 +52,19 @@ CodeMirror.defineMode('codeworld', (_config, modeConfig) => {
     const RE_INCOMMENT = /(?:[^{-]|-(?=$|[^}])|\{(?=$|[^-]))*/;
     const RE_ENDCOMMENT = /-}/;
 
+    const RE_OF = /of/;
+    const RE_LET = /let/;
+    const RE_IN = /in/;
+    const RE_WHERE = /where/;
+    const RE_DO = /do/;
+    const RE_CASE = /case/;
+
+    // Next non-comment or non-whitespace token
+    // is start of layout context
+    const TRIGGERED = 0;
+    // Parsing is not layout sensetive
+    const NORMAL = 1;
+
     function opening(bracket) {
         if (bracket === ')') return '(';
         if (bracket === ']') return '[';
@@ -59,14 +72,36 @@ CodeMirror.defineMode('codeworld', (_config, modeConfig) => {
         return bracket;
     }
 
+    // Start new layout context.
+    function checkContextStarter(stream, state) {
+        let match = null;
+        if (stream.match(RE_LET)   ||
+            stream.match(RE_WHERE) ||
+            stream.match(RE_DO)    ||
+            stream.match(RE_CASE)  ||
+            stream.match(RE_OF)
+           ) {
+            let nextChar = stream.peek();
+            // Check if it word like 'lettering' or 'offer'
+            if (nextChar === null || nextChar === undefined || /\s/.exec(nextChar)){
+                state.scanState = TRIGGERED;
+                return true;
+            }
+        }
+        return false;
+    }
+
     // The state has the following properties:
     //
     // func:          The function to tokenize the remaining stream.
     // commentLevel:  Number of levels of block comments.
-    // brackets:      Brackets, from outermost to innermost.
+    // layoutContext: Explicit brackets + implicit layout contexts.
+    // scanState:     When TRIGGERED - next token is start of layout context.
 
     function normal(stream, state) {
-        if (stream.match(RE_WHITESPACE)) return null;
+        let spanStyles = [];
+
+        if (stream.match(RE_WHITESPACE)) return [];
 
         if (stream.match(RE_STARTMETA)) {
             state.func = blockComment('meta');
@@ -82,43 +117,91 @@ CodeMirror.defineMode('codeworld', (_config, modeConfig) => {
 
         if (stream.match(RE_DASHES)) {
             stream.skipToEnd();
-            return 'comment';
+            return ['comment'];
         }
 
-        if (stream.match(RE_QUAL)) return 'qualifier';
-        if (stream.match(RE_VARID) || stream.match(RE_VARSYM)) return 'variable';
-        if (stream.match(RE_CONID) || stream.match(RE_CONSYM)) return 'variable-2';
-        if (stream.match(RE_NUMBER)) return 'number';
-        if (stream.match(RE_CHAR) || stream.match(RE_STRING)) return 'string';
+        if (checkContextStarter(stream, state)) {
+            return ['keyword'];
+        }
+
+        if (state.scanState === NORMAL && stream.match(RE_IN)) {
+            // Cancel previous layout 'let' context
+            state.layoutContext.pop();
+            return ['keyword'];
+        } else if (state.scanState === NORMAL) {
+            let lastContext = state.layoutContext[state.layoutContext.length - 1];
+            lastContext = lastContext === '{' ? 0 : lastContext;
+            if (stream.sol() && stream.indentation() < lastContext) {
+                // Close previous context
+                state.layoutContext.pop();
+            }
+            if (stream.column() == lastContext || stream.column() == 0) {
+                spanStyles.push('layout');
+            }
+        } else if (state.scanState === TRIGGERED && stream.match('{')) {
+            // Cancel previous layout context because of next char is '{'
+            state.layoutContext.pop();
+            state.scanState = NORMAL;
+        } else if (state.scanState === TRIGGERED) {
+            state.layoutContext.push(stream.column());
+            spanStyles.push('layout');
+            state.scanState = NORMAL;
+        }
+
+        if (stream.match(RE_QUAL)) {
+            spanStyles.push('qualifier');
+            return spanStyles;
+        };
+        if (stream.match(RE_VARID) || stream.match(RE_VARSYM)) {
+            spanStyles.push('variable');
+            return spanStyles;
+        };
+        if (stream.match(RE_CONID) || stream.match(RE_CONSYM)) {
+            spanStyles.push('variable-2');
+            return spanStyles;
+        };
+        if (stream.match(RE_NUMBER)) {
+            spanStyles.push('number');
+            return spanStyles;
+        };
+        if (stream.match(RE_CHAR) || stream.match(RE_STRING)) {
+            spanStyles.push('string');
+            return spanStyles;
+        };
 
         if (stream.match(RE_OPENBRACKET)) {
-            state.brackets.push(stream.current());
-            return `bracket${state.brackets.length <= 7 ? `-${  
-                state.brackets.length - 1}` : ''}`;
+            state.layoutContext.push(stream.current());
+            spanStyles.push(
+                `bracket${state.layoutContext.length <= 7 ? `-${
+                state.layoutContext.length - 1}` : ''}`);
+            return spanStyles;
         }
 
         if (stream.match(RE_CLOSEBRACKET)) {
-            const i = state.brackets.lastIndexOf(opening(stream.current()));
+            const i = state.layoutContext.lastIndexOf(opening(stream.current()));
             if (i < 0) {
-                return 'bracket';
+                spanStyles.push('bracket');
+                return spanStyles;
             } else {
-                while (state.brackets.length > i) state.brackets.pop();
-                return `bracket${state.brackets.length <= 6 ? `-${ 
-                    state.brackets.length}` : ''}`;
+                while (state.layoutContext.length > i) state.layoutContext.pop();
+                spanStyles.push(
+                    `bracket${state.layoutContext.length <= 6 ? `-${
+                    state.layoutContext.length}` : ''}`);
+                return spanStyles;
             }
         }
 
-        if (stream.eat(',')) return null;
+        if (stream.eat(',')) return [];
 
         stream.next();
-        return 'error';
+        return ['error'];
     }
 
     function blockComment(tokenType) {
         return (stream, state) => {
             if (state.commentLevel === 0) {
                 state.func = normal;
-                return tokenType;
+                return [tokenType];
             }
 
             stream.match(RE_INCOMMENT);
@@ -130,7 +213,7 @@ CodeMirror.defineMode('codeworld', (_config, modeConfig) => {
                 --state.commentLevel;
                 return state.func(stream, state);
             }
-            return tokenType;
+            return [tokenType];
         };
     }
 
@@ -170,17 +253,20 @@ CodeMirror.defineMode('codeworld', (_config, modeConfig) => {
             return {
                 func: normal,
                 commentLevel: 0,
-                brackets: []
+                layoutContext: [],
+                scanState: NORMAL
             };
         },
 
         token: (stream, state) => {
             const t = state.func(stream, state);
-            if (['variable', 'variable-2'].indexOf(t) !== -1) {
-                const w = stream.current();
-                if (wellKnownWords.hasOwnProperty(w)) return wellKnownWords[w];
-            }
-            return t;
+            const w = stream.current();
+            if (wellKnownWords.hasOwnProperty(w)) {
+                if (t.indexOf('variable') !== -1) t.pop(t.indexOf('variable'));
+                if (t.indexOf('variable-2') !== -1) t.pop(t.indexOf('variable-2'));
+                t.push(wellKnownWords[w]);
+            };
+            return t.join(' ');
         },
 
         blockCommentStart: '{-',

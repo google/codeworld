f4ad39892722afa87aeadbbc524835052b0da15a
Chris Smith <cdsmith@gmail.com>
2019-05-12 06:06:44 +0000

    Progress on #681
    This doesn't fix the problem, but it does fix the specific causes
    in the Wrapped type.
    
    I don't even really understand why this works.  There's some real
    oddness going on here with strictness.  Making all of the
    non-wrapped fields strict fixes the bug, but it should not be
    required.  I wish things made more sense.

--- codeworld-api/test/Driver.hs
+++ codeworld-api/test/Driver.hs
@@ -18,20 +18,19 @@ import System.Mem.StableName
 import Control.Concurrent
 import GHC.Prim
 
+checkWrappedIdentity :: (Wrapped a -> Wrapped a) -> Wrapped a -> Assertion
+checkWrappedIdentity f x = assertBool "identity" (identical x (f x))
+
 tests :: Test
 tests = testGroup "Driver"
-    [ testCase "toState preserves identity" $ do
-        let wrapped = wrappedInitial 42
-        let target = toState id wrapped
-        assertBool "" $ identical wrapped target
-    , testCase "wrappedStep preserves identity" $ do
-        -- Expected failure: See https://github.com/google/codeworld/issues/681
-        let wrapped = wrappedInitial 42
-        let target  = wrappedStep (const id) 1 wrapped
-        assertBool "" $ not $ identical wrapped target
-    , testCase "wrapping of shared identity is shared (events)" $ do
-        -- Expected failure: See https://github.com/google/codeworld/issues/681
-        let wrapped = wrappedInitial 42
-        let target  = wrappedEvent (const []) (const id) (const id) (TimePassing 0) wrapped
-        assertBool "" $ not $ identical wrapped target
+    [ testCase "toState preserves identity" $
+          checkWrappedIdentity (toState id) (wrappedInitial 42)
+    , testCase "wrappedStep preserves identity" $
+          checkWrappedIdentity (wrappedStep (const id) 1) (wrappedInitial 42)
+    , testCase "wrapping of shared identity is shared (events)" $
+          checkWrappedIdentity (wrappedEvent (const [])
+                                             (const id)
+                                             (const id)
+                                             (TimePassing 1))
+                               (wrappedInitial 42)
     ]

402e2888d2c27dea9878037d2f9bd350898b7df6
Chris Smith <cdsmith@gmail.com>
2019-06-16 03:51:36 +0000

    Add a TH utility to embed data-scheme urls into source.
    See #727

---
+++ codeworld-api/src/Util/EmbedAsUrl.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+{-
+  Copyright 2019 The CodeWorld Authors. All rights reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+-}
+
+module Util.EmbedAsUrl (embedAsUrl) where
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Base64 as B64
+import qualified Data.ByteString.Char8 as BC
+import Language.Haskell.TH.Syntax
+
+embedAsUrl :: String -> FilePath -> Q Exp
+embedAsUrl contentType f = do
+    qAddDependentFile f
+    payload <- runIO $ B64.encode <$> B.readFile f
+    let uri = "data:" <> BC.pack contentType <> ";base64," <> payload
+    [e| unsafePerformIO (
+            unsafePackAddressLen
+                $(return $ LitE $ IntegerL $ fromIntegral $ B.length uri)
+                $(return $ LitE $ StringPrimL $ B.unpack uri)) |]

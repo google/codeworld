4ff677bd98b9a0b9a87acd6634e08c9fe1cd92eb
Chris Smith <cdsmith@gmail.com>
2020-06-11 04:25:06 +0000

    Update copyright year on all files.

--- codeworld-game-server/src/CodeWorld/GameServer.hs
+++ codeworld-game-server/src/CodeWorld/GameServer.hs
@@ -6,7 +6,7 @@
 {-# LANGUAGE StandaloneDeriving #-}
 
 {-
-  Copyright 2019 The CodeWorld Authors. All rights reserved.
+  Copyright 2020 The CodeWorld Authors. All rights reserved.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
--- funblocks-client/src/Blockly/DesignBlock.hs
+++ funblocks-client/src/Blockly/DesignBlock.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE JavaScriptFFI #-}
 
 {-
-  Copyright 2019 The CodeWorld Authors. All Rights Reserved.
+  Copyright 2020 The CodeWorld Authors. All Rights Reserved.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.

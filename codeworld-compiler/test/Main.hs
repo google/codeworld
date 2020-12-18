692e2e5a57af9bf7c5ef5b192cdec5c658e2ad65
Chris Smith <cdsmith@gmail.com>
2020-12-18 15:56:56 +0000

    Improvements to test suite.
    Main addition is to run test cases in Haskell mode if the directory
    begins with `haskell_`

--- codeworld-compiler/test/Main.hs
+++ codeworld-compiler/test/Main.hs
@@ -23,6 +23,7 @@ import Data.List
 import System.Directory
 import System.Exit
 import System.FilePath.Posix
+import System.IO
 import System.IO.Temp
 import Test.Framework
 import Test.Framework.Providers.HUnit
@@ -30,34 +31,39 @@ import Test.HUnit hiding (Test)
 
 magicModuleFinder :: String -> FilePath -> String -> IO (Maybe FilePath)
 magicModuleFinder testName dir modName = do
-    cwd <- getCurrentDirectory
-    let file = "test/testcases" </> testName </> modName <.> "hs"
-    exists <- doesFileExist file
-    if exists then return (Just file) else return Nothing
+  cwd <- getCurrentDirectory
+  let file = "test/testcases" </> testName </> modName <.> "hs"
+  exists <- doesFileExist file
+  if exists then return (Just file) else return Nothing
 
 compilerOutput :: String -> IO String
 compilerOutput testName =
-    withSystemTempDirectory "cwtest" $ \dir -> do
-        compileSource
-            ErrorCheck
-            ("test/testcases" </> testName </> "source.hs")
-            (magicModuleFinder testName dir)
-            (dir </> "output.txt")
-            "codeworld"
-            False
-        readFile (dir </> "output.txt")
+  withSystemTempDirectory "cwtest" $ \dir -> do
+    compileSource
+      ErrorCheck
+      ("test/testcases" </> testName </> "source.hs")
+      (magicModuleFinder testName dir)
+      (dir </> "output.txt")
+      buildMode
+      False
+    readFile (dir </> "output.txt")
+  where
+    buildMode
+      | "haskell_" `isPrefixOf` testName = "haskell"
+      | otherwise = "codeworld"
 
 trim :: String -> String
 trim = dropWhile isSpace . dropWhileEnd isSpace
 
 toTestCase x = testCase x $ do
-    actual <- trim <$> compilerOutput x
-    expected <- trim <$> readFile ("test/testcases" </> x </> "expected_output.txt")
-    assertEqual x expected actual
+  hPutStrLn stderr $ "Testing: " ++ x
+  actual <- trim <$> compilerOutput x
+  expected <- trim <$> readFile ("test/testcases" </> x </> "expected_output.txt")
+  assertEqual x expected actual
 
 genTestCases :: [String] -> [Test]
 genTestCases [] = [testCase "Locating test cases" $ assertFailure "No test cases found."]
-genTestCases x  = map toTestCase x
+genTestCases x = map toTestCase x
 
 main :: IO ()
 main = defaultMain . genTestCases =<< listDirectory "test/testcases"

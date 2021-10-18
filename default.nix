06de44415c8635c364a467c61709794bace081ae
Peter Becich <peterbecich@gmail.com>
2021-10-17 22:33:16 -0700

    build subset of packages with ghcjs and Nix

--- default.nix
+++ default.nix
@@ -1,16 +1,20 @@
 # https://input-output-hk.github.io/haskell.nix/tutorials/getting-started/
-let
-  sources = import ./nix/sources.nix {};
-  haskellNix = import sources.haskellNix {};
-  pkgs = import
-    haskellNix.sources.nixpkgs-2105
-    haskellNix.nixpkgsArgs;
-in pkgs.pkgsCross.ghcjs.haskell-nix.project {
-#in pkgs.haskell-nix.project {
-  projectFileName = "cabal.project";
-  src = pkgs.haskell-nix.haskellLib.cleanGit {
-    name = "haskell-nix-project";
-    src = ./.;
-  };
-  compiler-nix-name = "ghc865";
-}
+{ compiler ? "ghc884",
+  ghcjs ? "ghcjs",
+  withCoverage ? false
+}:
+  let
+    sources = import ./nix/sources.nix {};
+    haskellNix = import sources.haskellNix {};
+    pkgs = import
+      haskellNix.sources.nixpkgs-2105
+      haskellNix.nixpkgsArgs;
+  in
+    pkgs.pkgsCross.ghcjs.haskell-nix.project {
+      projectFileName = "cabal.project";
+      src = pkgs.haskell-nix.haskellLib.cleanGit {
+        name = "codeworld";
+        src = ./.;
+      };
+      compiler-nix-name = compiler;
+    }
